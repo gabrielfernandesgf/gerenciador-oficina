7476d89815ee89813bb9688a887beecbb6251357
Gabriel Fernandes <129800907+gabrielfernandesgf@users.noreply.github.com>
2025-06-08 11:36:03 -0300

    Delete FrontEnd/AutoGynService directory
    foi criado um repository para o front

--- FrontEnd/AutoGynService/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-
-export const routes: Routes = [
-  // Demais rotas aqui
-
-  {path: "", redirectTo: "/home", pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
-];
